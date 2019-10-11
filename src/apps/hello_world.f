a0d34636aa93edf12f220206b7bb224bcf6e6d4c
Jack Maguire <jackmaguire1444@gmail.com>
2019-10-11 15:35:35 -0400

    single char printing, no newline

--- src/apps/hello_world.f
+++ src/apps/hello_world.f
@@ -2,5 +2,8 @@
 *     The PRINT statement is like WRITE,
 *     but prints to the standard output unit
       PRINT *, 'Hello'//achar(27)//'[95m World '//achar(27)//'[0m!'
+      do i=1,3
+         write(*, fmt="(a,i0)", advance="no") 'x'
+      end do
       STOP
       END
