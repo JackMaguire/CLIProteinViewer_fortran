83b0048e5010d91586f0c502ba90c0d69931fe33
Jack Maguire <jackmaguire1444@gmail.com>
2019-10-11 15:29:14 -0400

    src/apps/hello_world.f

---
+++ src/apps/hello_world.f
@@ -0,0 +1,6 @@
+      PROGRAM HELLO
+*     The PRINT statement is like WRITE,
+*     but prints to the standard output unit
+      PRINT *, 'Hello'//achar(27)//'[95m World '//achar(27)//'[0m!'
+      STOP
+      END
